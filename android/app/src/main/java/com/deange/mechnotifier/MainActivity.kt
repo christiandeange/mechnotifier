a650d6c967b39af0c93fe00e5b3089b66f9a90f0
Christian De Angelis <chr@squareup.com>
2021-04-11 20:43:43 -0400

    Update all dependencies to newer versions.
    The workflow version update deprecates the setContentWorkflow method of
    starting the workflow, recommending usage of ViewModels to persist it
    across configuration changes.

--- android/app/src/main/java/com/deange/mechnotifier/MainActivity.kt
+++ android/app/src/main/java/com/deange/mechnotifier/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.deange.mechnotifier
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import androidx.savedstate.SavedStateRegistryOwner
 import com.deange.mechnotifier.dagger.Scope
 import com.deange.mechnotifier.model.PostFilter
+import com.deange.mechnotifier.settings.SettingsScreen
 import com.deange.mechnotifier.settings.SettingsViewRegistry
 import com.deange.mechnotifier.settings.SettingsWorkflow
 import com.deange.mechnotifier.settings.SettingsWorkflowProps
 import com.deange.mechnotifier.topics.Topic
 import com.deange.mechnotifier.topics.TopicCreator
 import com.deange.mechnotifier.topics.TopicRepository
-import com.squareup.workflow1.ui.WorkflowRunner
-import com.squareup.workflow1.ui.setContentWorkflow
+import com.squareup.workflow1.ui.WorkflowLayout
+import com.squareup.workflow1.ui.renderWorkflowIn
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -23,6 +34,10 @@ class MainActivity : AppCompatActivity() {
   @Inject lateinit var settingsWorkflow: SettingsWorkflow
   @Inject lateinit var topicRepository: TopicRepository
   @Inject lateinit var topicCreator: TopicCreator
+
+  private val viewModel: MainViewModel by viewModels {
+    MainViewModelFactory(this, settingsWorkflow, intent.extras)
+  }
 
   private lateinit var scope: Scope
 
@@ -47,26 +62,60 @@ class MainActivity : AppCompatActivity() {
   ) {
     val (region, subregion) = topics.mapNotNull { topicCreator.toRegionOrNull(it) }.first()
     val publicTypes = topics.mapNotNull { topicCreator.toPublicTypeOrNull(it) }.toSet()
-
-    setContentWorkflow(
-      registry = settingsViewRegistry,
-      configure = {
-        WorkflowRunner.Config(
-          workflow = settingsWorkflow,
-          props = SettingsWorkflowProps(
-            region = region,
-            subregion = subregion,
-            publicTypes = publicTypes,
-            postFilter = postFilter
-          )
-        )
-      },
-      onResult = { finishAfterTransition() }
+    val props = SettingsWorkflowProps(
+      region = region,
+      subregion = subregion,
+      publicTypes = publicTypes,
+      postFilter = postFilter
     )
+
+    setContentView(
+      WorkflowLayout(this).apply {
+        start(viewModel.render(props), settingsViewRegistry)
+      }
+    )
+
+    lifecycleScope.launch {
+      viewModel.waitForExit()
+      finishAfterTransition()
+    }
   }
 
   override fun onDestroy() {
     scope.destroy()
     super.onDestroy()
   }
+}
+
+class MainViewModelFactory(
+  owner: SavedStateRegistryOwner,
+  private val workflow: SettingsWorkflow,
+  defaultArgs: Bundle? = null
+) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+
+  @Suppress("UNCHECKED_CAST")
+  override fun <T : ViewModel> create(
+    key: String,
+    modelClass: Class<T>,
+    handle: SavedStateHandle
+  ): T = MainViewModel(handle, workflow) as T
+}
+
+class MainViewModel(
+  private val savedState: SavedStateHandle,
+  private val workflow: SettingsWorkflow
+) : ViewModel() {
+  private val running = Job()
+
+  fun render(props: SettingsWorkflowProps): Flow<SettingsScreen> {
+    return renderWorkflowIn(
+      workflow = workflow,
+      prop = props,
+      scope = viewModelScope,
+      savedStateHandle = savedState,
+      onOutput = { running.complete() }
+    )
+  }
+
+  suspend fun waitForExit() = running.join()
 }
